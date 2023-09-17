14faa2212c4e5dba7fd99a6526c6937414f9c109
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2023-09-17 13:33:08 -0500

    update some files

--- analysis/Bittree-Dmap/dmap.cxx
+++ analysis/Bittree-Dmap/dmap.cxx
@@ -1,5 +1,5 @@
 // C++ program to create an empty 
-// vector and push values one
+// pmapor and push values one
 // by one.
 #include <iostream>
 #include <AMReX_Vector.H>
@@ -10,28 +10,26 @@ using namespace std;
 int main()
 {
     // Create an empty vector
-    amrex::Vector<int> vect[3];
-  
-    vect[0].push_back(0);
-    vect[0].push_back(1);
-    vect[0].push_back(2);
-    vect[0].push_back(3);
-    vect[1].push_back(4);
-    vect[1].push_back(5);
-    vect[1].push_back(6);
-    vect[1].push_back(7);
-    vect[2].push_back(8);
-    vect[2].push_back(9);
-    vect[2].push_back(10);
-    vect[2].push_back(11);
+    amrex::Vector<int> pmap;
+ 
+    pmap.push_back(0);
+    pmap.push_back(0);
+    pmap.push_back(0);
+    pmap.push_back(0);
+    pmap.push_back(1);
+    pmap.push_back(1);
+    pmap.push_back(1);
+    pmap.push_back(1);
+    pmap.push_back(2);
+    pmap.push_back(2);
+    pmap.push_back(2);
+    pmap.push_back(2);
 
-    const amrex::Vector<int>& pmap = vect[2];
-  
     for (int x : pmap) 
        cout << x << " ";
  
     amrex::DistributionMapping dm;
-    dm = amrex::DistributionMapping(pmap);
+    dm.define(pmap);
 
     return 0;
 }
