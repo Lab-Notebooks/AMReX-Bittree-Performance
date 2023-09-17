bb24075327b692ce5a46a8213e2d67cee8388bf1
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2023-09-16 22:52:38 -0500

    improve logic

--- analysis/Bittree-Dmap/dmap.cxx
+++ analysis/Bittree-Dmap/dmap.cxx
@@ -10,13 +10,22 @@ using namespace std;
 int main()
 {
     // Create an empty vector
-    amrex::Vector<int> vect;
+    amrex::Vector<int> vect[3];
   
-    vect.push_back(0);
-    vect.push_back(1);
-    vect.push_back(2);
+    vect[0].push_back(0);
+    vect[0].push_back(1);
+    vect[0].push_back(2);
+    vect[0].push_back(3);
+    vect[1].push_back(4);
+    vect[1].push_back(5);
+    vect[1].push_back(6);
+    vect[1].push_back(7);
+    vect[2].push_back(8);
+    vect[2].push_back(9);
+    vect[2].push_back(10);
+    vect[2].push_back(11);
 
-    const amrex::Vector<int>& pmap = vect;
+    const amrex::Vector<int>& pmap = vect[2];
   
     for (int x : pmap) 
        cout << x << " ";
