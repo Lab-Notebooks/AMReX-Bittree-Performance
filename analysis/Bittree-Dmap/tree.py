90919219387e82707919c43d5ac4174ddd0e762c
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2023-09-18 16:59:37 -0500

    update dmap test script

--- analysis/Bittree-Dmap/tree.py
+++ analysis/Bittree-Dmap/tree.py
@@ -1,39 +1,17 @@
 import numpy
 
-nprocs = 10
+tree = numpy.arange(16, 376, 1, dtype=int)
 
-class Block:
-    def __init__(self, level, leaf, myid):
-        self.level = level
-        self.leaf = leaf
-        self.id = myid
+nprocs = 11
+div = int(len(tree) / nprocs)
+mod = len(tree) % nprocs
 
+print(len(tree), nprocs, div, mod)
 
-tree = [
-    Block(0, 1, 0),
-    Block(0, 1, 1),
-    Block(0, 1, 2),
-    Block(0, 0, 3),
-    Block(1, 1, 4),
-    Block(1, 1, 5),
-    Block(1, 1, 6),
-    Block(1, 0, 7),
-    Block(2, 1, 8),
-    Block(2, 1, 9),
-    Block(2, 1, 10),
-    Block(2, 0, 11),
-    Block(3, 1, 12),
-    Block(3, 1, 13),
-    Block(3, 1, 14),
-    Block(3, 0, 15),
-]
-
-div = int(len(tree)/nprocs)
-mod = len(tree)%nprocs
-
-dmap = numpy.array([-1]*len(tree))
-
+dmap_proc = numpy.array([-1] * len(tree))
 for proc in range(nprocs):
-    dmap[proc*div+min(proc, mod):(proc+1)*div+min(proc+1, mod)] = proc
+    dmap_proc[proc * div + min(proc, mod) : (proc + 1) * div + min(proc + 1, mod)] = proc
 
-print(dmap)
+dmap_tree = numpy.array([-1] * len(tree))
+for block in range(len(tree)):
+    dmap_tree[block] = 0
